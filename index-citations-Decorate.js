ec9e22c37ac65c4fc5c644f5f5934c009fb3f0d2
thesunshade <82448383+thesunshade@users.noreply.github.com>
2022-01-14 19:03:49 +0530

    Add files via upload

--- index-citations-Decorate.js
+++ index-citations-Decorate.js
@@ -2,6 +2,32 @@ import { dictionary } from "./dictionary.js";
 
 let decorated = [...dictionary];
 decorated.forEach(item => {
+  // VINAYA
+  //
+  // Volumes 3 and 4 do not track exactly to pli-tv-bu-vb and pli-tv-bu-vb
+  // ??? Will we have to do this programatically???
+  ///
+  item.text = item.text
+    .replace(
+      /<span class='ref'>Vin\.i\.(\d+)(.*)<\/span>/g,
+      "<a class='ref' data-division='/vinaya/pli-tv-kd' data-reference='pts-vp-pli1.$1' data-url='en/brahmali'>AN.i.$1$2</a>"
+    )
+    .replace(
+      /<span class='ref'>Vin\.ii\.(\d+)(.*)<\/span>/g,
+      "<a class='ref' data-division='/vinaya/pli-tv-kd' data-reference='pts-vp-pli2.$1' data-url='en/brahmali'>AN.ii.$1$2</a>"
+    )
+    .replace(
+      /<span class='ref'>Vin\.iii\.(\d+)(.*)<\/span>/g,
+      "<a class='ref' data-division='/vinaya/XXXXXXXXX' data-reference='pts-vp-pli3.$1' data-url='en/brahmali'>AN.iii.$1$2</a>"
+    )
+    .replace(
+      /<span class='ref'>Vin\.iv\.(\d+)(.*)<\/span>/g,
+      "<a class='ref' data-division='/vinaya/XXXXXXXXX' data-reference='pts-vp-pli4.$1' data-url='en/brahmali'>AN.iv.$1$2</a>"
+    )
+    .replace(
+      /<span class='ref'>Vin\.v\.(\d+)(.*)<\/span>/g,
+      "<a class='ref' data-division='/vinaya/pli-tv-pvr' data-reference='pts-vp-pli5.$1' data-url='en/brahmali'>AN.v.$1$2</a>"
+    );
   // DIGHA NIKAYA
   item.text = item.text
     .replace(
@@ -75,6 +101,9 @@ decorated.forEach(item => {
       "<a class='ref' data-division='/sutta/an' data-reference='pts-vp-pli5.$1' data-url='/en/sujato'>AN.v.$1$2</a>"
     );
   // DHAMMAPADA
+  //
+  // So, I wrongly assumed that the Dhp followed the same convention as Snp. However `vnp` doesn't seem to exist in the Dhp files. So this below is wrong.
+  //
   item.text = item.text.replace(
     /<span class='ref'>Dhp\.(\d+)(.*)<\/span>/g,
     "<a class='ref' data-division='/sutta/kn/dhp' data-reference='vnp.$1' data-url='/en/sujato'>Dhp.$1$2</a>"
@@ -83,6 +112,57 @@ decorated.forEach(item => {
   item.text = item.text.replace(
     /<span class='ref'>Snp\.(\d+)(.*)<\/span>/g,
     "<a class='ref' data-division='/sutta/kn/snp' data-reference='vnp.$1' data-url='/en/sujato'>Snp.$1$2</a>"
+  );
+  // UDANA
+  item.text = item.text.replace(
+    /<span class='ref'>Snp\.(\d+)(.*)<\/span>/g,
+    "<a class='ref' data-division='/sutta/kn/ud' data-reference='pts-vp-pli$1' data-url='/en/sujato'>Ud.$1$2</a>"
+  );
+  // ITIVUTTAKA
+  item.text = item.text.replace(
+    /<span class='ref'>Snp\.(\d+)(.*)<\/span>/g,
+    "<a class='ref' data-division='/sutta/kn/iti' data-reference='pts-vp-pli$1' data-url='/en/sujato'>Iti.$1$2</a>"
+  );
+  // THERAGATHA
+  item.text = item.text.replace(
+    /<span class='ref'>Thag\.(\d+)(.*)<\/span>/g,
+    "<a class='ref' data-division='/sutta/kn/thag' data-reference='vnp.$1' data-url='/en/sujato'>Thag.$1$2</a>"
+  );
+  // THERIGATHA
+  item.text = item.text.replace(
+    /<span class='ref'>Thig\.(\d+)(.*)<\/span>/g,
+    "<a class='ref' data-division='/sutta/kn/thig' data-reference='vnp.$1' data-url='/en/sujato'>Thig.$1$2</a>"
+  );
+  // JATAKA
+  item.text = item.text
+    .replace(
+      /<span class='ref'>Ja\.i\.(\d+)(.*)<\/span>/g,
+      "<a class='ref' data-division='/sutta/ja' data-reference='pts-vp-pli1.$1' data-url='/pli/ms'>Ja.i.$1$2</a>"
+    )
+    .replace(
+      /<span class='ref'>Ja\.ii\.(\d+)(.*)<\/span>/g,
+      "<a class='ref' data-division='/sutta/ja' data-reference='pts-vp-pli2.$1' data-url='/pli/ms'>Ja.ii.$1$2</a>"
+    )
+    .replace(
+      /<span class='ref'>Ja\.iii\.(\d+)(.*)<\/span>/g,
+      "<a class='ref' data-division='/sutta/ja' data-reference='pts-vp-pli3.$1' data-url='/pli/ms'>Ja.iii.$1$2</a>"
+    )
+    .replace(
+      /<span class='ref'>Ja\.iv\.(\d+)(.*)<\/span>/g,
+      "<a class='ref' data-division='/sutta/ja' data-reference='pts-vp-pli4.$1' data-url='/pli/ms'>Ja.iv.$1$2</a>"
+    )
+    .replace(
+      /<span class='ref'>Ja\.v\.(\d+)(.*)<\/span>/g,
+      "<a class='ref' data-division='/sutta/ja' data-reference='pts-vp-pli5.$1' data-url='/pli/ms'>Ja.v.$1$2</a>"
+    )
+    .replace(
+      /<span class='ref'>Ja\.vi\.(\d+)(.*)<\/span>/g,
+      "<a class='ref' data-division='/sutta/ja' data-reference='pts-vp-pli6.$1' data-url='/pli/ms'>Ja.vi.$1$2</a>"
+    );
+  // Milindapanha
+  item.text = item.text.replace(
+    /<span class='ref'>Mil\.(\d+)(.*)<\/span>/g,
+    "<a class='ref' data-division='/sutta/kn/mil' data-reference='pts-vp-pli$1' data-url='/pli/ms'>Mil.$1$2</a>"
   );
 });
 console.log(decorated);
